74b851096bb75b7bfc892cba72c1b434eaa1d07e
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-06-30 11:41:42 -0300

    Redux adicionado ao projeto - User Reducer adicionado

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,29 +1,56 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import UserActionTypes from "../../redux/user/action-types";
 
 // Components
 import Cart from "../cart/index";
 
 // Styles
 import * as Styles from "./styles";
+import { loginUser, logoutUser } from "../../redux/user/actios";
 
 function Header() {
-  const [cartIsVisible, setCartIsVisible] = useState(false);
+    const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  const handleCartClick = () => {
-    setCartIsVisible(true);
-  };
+    const { currentUser } = useSelector(
+        (rootReducer) => rootReducer.userReducer
+    );
+    const dispatch = useDispatch();
 
-  return (
-    <Styles.Container>
-      <Styles.Logo>Redux Shopping</Styles.Logo>
-      <Styles.Buttons>
-        <div>Login</div>
-        <div onClick={handleCartClick}>Carrinho</div>
-      </Styles.Buttons>
+    console.log("Current User: " + currentUser);
 
-      <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
-    </Styles.Container>
-  );
+    const handleCartClick = () => {
+        setCartIsVisible(true);
+    };
+
+    const handleLoginClick = () => {
+        dispatch(
+            loginUser({
+                username: "zRavenclaw_",
+                email: "jackson.monteiro5619@gmail.com",
+            })
+        );
+    };
+
+    const handleLogoutClick = () => {
+        dispatch(logoutUser);
+    };
+
+    return (
+        <Styles.Container>
+            <Styles.Logo>Redux Shopping</Styles.Logo>
+            <Styles.Buttons>
+                {currentUser ? (
+                    <div onClick={handleLogoutClick}>Sair</div>
+                ) : (
+                    <div onClick={handleLoginClick}>Login</div>
+                )}
+                <div onClick={handleCartClick}>Carrinho</div>
+            </Styles.Buttons>
+
+            <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
+        </Styles.Container>
+    );
 }
 
 export default Header;
